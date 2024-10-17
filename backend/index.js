7b1611eecd2932c060685020206fb9a35c313bca
RAJDEEPASAHA <142664697+RAJDEEPASAHA@users.noreply.github.com>
2024-10-17 23:27:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser')
 
 const app=express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : "https://ecommerce-mern-rajdeepa24.netlify.app",
     credentials : true
 }))
 app.use(express.json())
